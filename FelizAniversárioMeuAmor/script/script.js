ee277a5ac2356299840b4768eb5432ba91301bd5
Diniz <132007161+JDINIZ15@users.noreply.github.com>
2026-05-09 12:24:09 -0300

    Add files via upload

---
+++ FelizAniversárioMeuAmor/script/script.js
@@ -0,0 +1,17 @@
+let btn = document.querySelector(".form_btn");
+
+
+btn.addEventListener("click", () =>{
+  let nome = document.querySelector(".nome_usuario").value;
+  let senha = document.querySelector(".senha_usuario").value;
+
+  nome = nome.toLowerCase()
+
+  if(nome == "clara" && senha == "0905"){
+    window.location.href = "/html/EuteAmo.html";
+  }
+  else{
+    alert("Sai Fora, intruso!")
+    location.reload();
+  }
+})
